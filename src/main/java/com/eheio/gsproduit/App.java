8c3c21c04303c386503d6c974da43536872d66fa
ImaneDKHISSI <imane.dk99@gmail.com>
2024-01-12 11:24:14 +0100

    Initialisation du Projet Maven

---
+++ src/main/java/com/eheio/gsproduit/App.java
@@ -0,0 +1,13 @@
+package com.eheio.gsproduit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
